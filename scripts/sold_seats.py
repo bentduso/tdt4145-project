b0081497341ce7045705bca2fd3fa181ca1b6ec4
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-03-15 18:47:16 +0100

    feat: generalised the script so we only need one script for both theater plays

--- scripts/sold_seats.py
+++ scripts/sold_seats.py
@@ -1,7 +1,10 @@
 import sqlite3
-def process_gamle_scene(file_path, hall_id, theater_play_id):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
+from datetime import datetime
+conn = sqlite3.connect('db.sqlite3')
+cursor = conn.cursor()
+transaction_id = 1  
+def process_sold_seats(file_path, hall_id, theater_play_id):
+    global transaction_id
     with open(file_path, 'r') as file:
         lines = file.readlines()
     
@@ -17,9 +20,8 @@ def process_gamle_scene(file_path, hall_id, theater_play_id):
 
     current_area = ""
     current_row = 0
-    transaction_id = 1  
-
-    
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    current_time = datetime.now().strftime("%H:%M:%S")
     for line in lines[1:]:  # Hopper over dato
         line = line.strip()
         if line in ["Galleri", "Balkong", "Parkett"]:
@@ -42,8 +44,12 @@ def process_gamle_scene(file_path, hall_id, theater_play_id):
                     if seat == '1':
                         cursor.execute("INSERT INTO ticket (show_date, theater_play_id, transaction_id, ticket_group_name, chair_number, chair_row, area) VALUES (?, ?, ?, 'Voksen', ?, ?, ?)", (show_date, theater_play_id, transaction_id, chair_number, current_row, current_area))
                         conn.commit()
+                        cursor.execute("INSERT INTO ticket_transaction (transaction_id, theater_play_id, customer_id, show_date, transaction_date, transaction_time) VALUES (?, ?, ?, ?, ?, ?)", (transaction_id, theater_play_id, 1, show_date, current_date, current_time))
+                        conn.commit()
                         transaction_id += 1
-    conn.close()
 
-process_gamle_scene('../project/gamle-scene.txt', 2, 2)
 
+    
+process_sold_seats('../project/hovedscenen.txt', 1, 1)
+process_sold_seats('../project/gamle-scene.txt', 2, 2)
+conn.close()
