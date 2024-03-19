327de46cf9c0557c3b2c5b5f5a97ea74be806ada
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-03-19 18:56:25 +0100

    feat: added total price. Also fixed grouping of chair rows to now group on area as well. Also did some cleanup

--- project/purchase_tickets.py
+++ project/purchase_tickets.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from utils import create_connection
 
 
-def buy_tickets(database_path, name, show_date):
-    conn = create_connection(database_path)
+def buy_tickets(conn, name, show_date):
     cursor = conn.cursor()
     current_date = datetime.now().strftime("%Y-%m-%d")
     current_time = datetime.now().strftime("%H:%M:%S")
-
+    global total_price    
     cursor.execute(
         """
         SELECT chair_row, area
@@ -18,7 +17,7 @@ def buy_tickets(database_path, name, show_date):
         WHERE is_available = 1
             AND theater_play.name = ?
             AND show.show_date = ?
-        GROUP BY chair_row
+        GROUP BY chair_row, area
         HAVING COUNT(*) >= 9
         LIMIT 1
         """,
@@ -29,7 +28,7 @@ def buy_tickets(database_path, name, show_date):
         chair_row = row[0]
         cursor.execute(
             """
-            SELECT chair_number, area
+            SELECT chair_number, area, chair_row
             FROM chair
             WHERE is_available = 1 AND chair_row = ? AND area = ?
             LIMIT 9
@@ -38,7 +37,8 @@ def buy_tickets(database_path, name, show_date):
         chairs = cursor.fetchall()
         for chair in chairs:
             create_seat_transaction(cursor, chair, chair_row, name, show_date, current_date, current_time)
-        print("Tickets bought for chairs:", chairs)
+        
+        print("Tickets bought for chairs:", chairs, "Total price:", total_price, "NOK")
     else:
         print("There are no rows with 9 or more available chairs.")
     conn.close()
@@ -49,7 +49,7 @@ def buy_tickets(database_path, name, show_date):
 def create_seat_transaction(cursor, chair, chair_row, name, show_date, current_date, current_time):
     chair_number = chair[0]
     area = chair[1]
-
+    global total_price
     cursor.execute(
         """
         SELECT transaction_id
@@ -94,5 +94,30 @@ def create_seat_transaction(cursor, chair, chair_row, name, show_date, current_d
             AND area = ?
         """,
         (chair_row, chair_number, area))
+    
+    cursor.execute(
+        """
+        SELECT price
+        FROM ticket_group
+        WHERE ticket_group_name = "Ordinary" AND theater_play_id = ?
+        GROUP BY ticket_group_name
+        """,
+        (theater_play_id,))
+    total_price += cursor.fetchone()[0]
 
     cursor.connection.commit()
+
+def main():
+    conn = create_connection('../database/theater.db')
+
+    if conn is not None:
+        buy_tickets(conn, 'Størst av alt er kjærligheten', '2024-02-03')
+        
+        conn.close()
+    else:
+        print('Error! Cannot establish a database connection.')
+
+if __name__ == '__main__':
+    total_price = 0
+    main()
+    
