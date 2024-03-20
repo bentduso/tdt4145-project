7da46962a58157441c8a9d25ecd029590c0eae88
peteroj <peteroj@stud.ntnu.no>
2024-03-20 09:30:04 +0100

    Per nå så skal den fungere med dataen lagt til fra scriptet som sjekker billetter solgt den 3. mars. Kan legges til if statements for å sikre at brukeren vet hva som er galt med inputsa, likens som tidligere i programmet.

---
+++ project/sold_tickets_date.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect('theater.db')
+cursor = conn.cursor()
+
+# Dato input
+date = input("Skriv en ønsket dato i følgende format (YYYY-MM-DD): ")
+
+query = """
+    SELECT tp.name AS performance_name, 
+           COUNT(t.ticket_id) AS sold_tickets
+    FROM theater_play tp
+    LEFT JOIN show s
+        ON tp.theater_play_id = s.theater_play_id
+    LEFT JOIN ticket_transaction tt
+        ON s.show_date = tt.show_date AND tp.theater_play_id = tt.theater_play_id
+    LEFT JOIN ticket t
+        ON tt.transaction_id = t.transaction_id
+    WHERE s.show_date = ?
+    GROUP BY tp.theater_play_id, tp.name
+"""
+
+cursor.execute(query, (date,))
+
+results = cursor.fetchall()
+for row in results:
+    performance_name, sold_tickets = row
+    print(f"Stykke: {performance_name}, Antall solgte billetter: {sold_tickets} stk.")
+
+conn.close()
